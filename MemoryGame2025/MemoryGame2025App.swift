bfd4b8ce13e1b05fb893a43dff3303a67086b5b9
teycheng <pagnavathtey@gmail.com>
2025-01-10 11:08:24 -0800

    Initial Commit

---
+++ MemoryGame2025/MemoryGame2025App.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame2025App.swift
+//  MemoryGame2025
+//
+//  Created by ICS 224 on 2025-01-10.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGame2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGame2025Tests/MemoryGame2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGame2025Tests.swift
+//  MemoryGame2025Tests
+//
+//  Created by ICS 224 on 2025-01-10.
+//
+
+import Testing
+@testable import MemoryGame2025
+
+struct MemoryGame2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
