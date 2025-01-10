d632378229c07a4a71f88d1f8fa51a35467c7d38
teycheng <pagnavathtey@gmail.com>
2025-01-10 13:33:24 -0800

    completed coding

--- MemoryGame2025/ContentView.swift
+++ MemoryGame2025/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State private var showSun = false
+   @State private var showBird = true
+   @State private var showFish = false
+  @State private var count = 0
+   @State private var images = ["cloud.sun.rain", "bird","fish"];
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Image(systemName: "arrowtriangle.left")
+               .imageScale(.large)
+               .foregroundStyle(.tint)
+               .onTapGesture() {
+                   count -= 1
+                   if (count<0) {
+                       count = 2
+                   }
+               }
+                VStack {
+                    Image(systemName: images[count])
+                        .resizable()
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                        .aspectRatio(contentMode: ContentMode.fit)
+                    Text(images[count])
+                }
+                Image(systemName: "arrowtriangle.right")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .onTapGesture {
+                        count += 1
+                        if (count>2) {
+                            count = 0
+                        }
+                    }
         }
         .padding()
     }
