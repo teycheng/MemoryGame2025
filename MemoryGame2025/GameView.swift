fc07a3abd2a78d7d1111ec134c87c266f03d253d
teycheng <pagnavathtey@gmail.com>
2025-01-17 12:23:40 -0800

    create SettingView,GameView

---
+++ MemoryGame2025/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  MemoryGame2025
+//
+//  Created by ICS 224 on 2025-01-17.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
---
+++ MemoryGame2025/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MemoryGame2025
+//
+//  Created by ICS 224 on 2025-01-17.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
