9be307d28a204a2797d15fec19d4d36172a2157f
teycheng <pagnavathtey@gmail.com>
2025-01-17 11:23:45 -0800

    created a new ImagePickerView.

---
+++ MemoryGame2025/ImagePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerView.swift
+//  MemoryGame2025
+//
+//  Created by ICS 224 on 2025-01-17.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerView()
+}
